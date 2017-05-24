00294e7e5459d688e4f914b7a74b421e284f5123
HiiYL <yonglian146@gmail.com>
2017-05-24 18:19:41 +0800

    Added parallel linear layers for each glimpse

--- models/classification_models.py
+++ models/classification_models.py
@@ -28,11 +28,7 @@ class G_Spatial(nn.Module):
         self.hidden_size = hidden_size
         self.embed_size = embed_size
 
-        self.embed = nn.Sequential(
-            nn.Linear(self.question_vocab_size, embed_size),
-            nn.Dropout(0.5),
-            nn.ReLU()
-        )
+        self.embed = nn.Embedding(self.question_vocab_size, embed_size)
 
         self.lstm    = LSTMCustom(embed_size, hidden_size)
         #self.lstm2   = LSTMCustom(embed_size, hidden_size)
@@ -60,15 +56,7 @@ class G_Spatial(nn.Module):
     def forward(self, features, captions, lengths, states):
         """Decode image feature vectors and generates captions."""
         features_global, features_local = self.encode_fc(features)
-        if isinstance(captions, tuple):
-            captions, batch_sizes = captions
-            embeddings = self.embed(captions)
-            embeddings = pad_packed_sequence((embeddings,batch_sizes), batch_first=True)[0]
-        else:
-            batch_size, caption_length, _ = captions.size()
-            captions = captions.view(-1,self.question_vocab_size)
-            embeddings = self.embed(captions)
-            embeddings = embeddings.view(batch_size, caption_length, -1)
+        embeddings = self.embed(captions)
 
         embeddings = torch.cat((features_global.unsqueeze(1), embeddings), 1)
         batch_size = features_global.size(0)
@@ -115,7 +103,10 @@ class MultimodalRNN(nn.Module):
             nn.Linear(1200, len_vocab)
         )
         
-
+        self.classifier_qtype = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(1200, 65)
+        )
         self._initialize_weights()
 
     def _initialize_weights(self):
@@ -132,8 +123,9 @@ class MultimodalRNN(nn.Module):
         h3 = self.block3(v,h2)
 
         out = self.classifier(h3)
+        out_type = self.classifier_qtype(h3)
 
-        return out
+        return out, out_type
 
 
 class MultimodalBlock(nn.Module):
@@ -175,5 +167,198 @@ class MultimodalBlock(nn.Module):
         return out
 
 
+class MultimodalAttentionRNN(nn.Module):
+    def __init__(self, len_vocab, glimpse=4):
+        super(MultimodalAttentionRNN, self).__init__()
+        self.block1 = MLBBlock(2400, 1200, glimpse)
+        #self.block2 = MLBBlock(1200 * glimpse, 1200, glimpse)
+        #self.block3 = MLBBlock(1200, 1200, glimpse)
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(1200 * glimpse, len_vocab)
+        )
+        
+        #self.classifier_qtype = nn.Sequential(
+        #    nn.Dropout(),
+        #    nn.Linear(1200 * glimpse, len_vocab)
+        #)
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data.uniform_(-0.08, 0.08)#kaiming_uniform(m.weight.data)
+                m.bias.data.zero_()
+
+    def forward(self,v,q):
+
+        x = self.block1(v,q)
+        #x = self.block2(v,x)
+        #x = self.block3(v,x)
+        out = self.classifier(x)
+        #out_type = self.classifier_qtype(h3)
+
+        return out#, out_type
 
 
+class MultimodalAttentionBlock(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(MultimodalAttentionBlock, self).__init__()
+        self.visual_block = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(2048,2048),
+            nn.Tanh(),
+            nn.Dropout(),
+            nn.Linear(2048,output_size),
+            nn.Tanh(),
+            
+        )
+        self.attn_block = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(input_size,64),
+            nn.Softmax(),
+        )
+        self.question_block = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(input_size,output_size),
+            nn.Tanh(),
+        )
+        self.linear = nn.Linear(input_size,output_size)
+        self._initialize_weights()
+
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data.uniform_(-0.08, 0.08)#kaiming_uniform(m.weight.data)
+                m.bias.data.zero_()
+
+    def forward(self,v, q):
+        attn = self.attn_block(q)
+        v = torch.bmm(v.view(v.size(0), v.size(1), -1), attn.unsqueeze(2)).squeeze(2)
+
+        V = self.visual_block(v)
+        Q = self.question_block(q)
+
+        combined = V * Q
+
+        q_skip = self.linear(q)
+        out = q_skip + combined
+
+        return out
+
+
+class MLBBlock(nn.Module):
+    def __init__(self, input_size, output_size, glimpse):
+        super(MLBBlock, self).__init__()
+
+        self.glimpse = glimpse
+        self.visual_attn = nn.Sequential(
+            nn.Conv2d(2048, 1200, kernel_size=1,stride=1,padding=0),
+            nn.Tanh()
+        )
+        self.attn_block = nn.Conv2d(1200, glimpse, kernel_size=1,stride=1,padding=0)
+
+        self.question_block_attn = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(input_size,output_size),
+            nn.Tanh(),
+        )
+
+        glimpses = []
+        for i in range(glimpse):
+            glimpses.append(
+                nn.Sequential(
+                    nn.Dropout(),
+                    nn.Linear(2048,output_size),
+                    nn.Tanh()
+                )
+            )
+        self.glimpses = ListModule(*glimpses)
+
+        self.question_block = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(input_size,output_size * glimpse),
+            nn.Tanh(),
+        )
+        self._initialize_weights()
+
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.weight.data.uniform_(-0.08, 0.08)#kaiming_uniform(m.weight.data)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Conv2d):
+                m.weight.data.uniform_(-0.08, 0.08)
+                m.bias.data.zero_()
+
+    def forward(self,v, q):
+        v_atten  = self.visual_attn(v)
+        # 200 x 1200 x 8 x 8
+        q_atten  = self.question_block_attn(q)
+        # 200 x 1200
+
+        batch_size, depth, height, width = v_atten.size()
+
+        v_atten     = v_atten.view(batch_size, depth, -1)
+        # 200 x 1200 x 64
+        q_replicate = q_atten.unsqueeze(2).expand_as(v_atten)
+        # 200 x 1200 x 64
+
+        attn = v_atten * q_replicate
+        attn = attn.view(batch_size, depth, height, width)
+        # 200 x 1200 x 8 x 8
+        attn = self.attn_block(attn)
+        # 200 x glimpse x 8 x 8
+        attn = attn.view(attn.size(0),attn.size(1), -1)
+        # 200 x glimpse x 64
+        
+        # apply softmax along each glimpse slice
+        # b - batch
+        # s - glimpse_slice
+        # d - depth ( spatial features )
+        b, s, d = attn.size()
+        attn = F.softmax(attn.view(b * s, d)).view(b, s, d)
+        # attn - 200 x 64 x glimpse
+
+        v    = v.view(v.size(0), v.size(1), -1)
+        # 200 x 2048 x 64
+        v    = v.permute(0,2,1).contiguous()
+        # 200 x 64 x 2048 
+
+        v    = torch.bmm(attn, v)
+        # 200 x glimpse x 2048
+
+        b, s, d = v.size()
+        V = torch.cat([ self.glimpses[i](v[:,i,:]) for i in range(s) ], 1)
+        # 200 x glimpse * 1200
+        Q = self.question_block(q)
+
+        out = V * Q
+
+        return out
+
+
+class ListModule(nn.Module):
+    def __init__(self, *args):
+        super(ListModule, self).__init__()
+        idx = 0
+        for module in args:
+            self.add_module(str(idx), module)
+            idx += 1
+
+    def __getitem__(self, idx):
+        if idx < 0 or idx >= len(self._modules):
+            raise IndexError('index {} is out of range'.format(idx))
+        it = iter(self._modules.values())
+        for i in range(idx):
+            next(it)
+        return next(it)
+
+    def __iter__(self):
+        return iter(self._modules.values())
+
+    def __len__(self):
+        return len(self._modules)
